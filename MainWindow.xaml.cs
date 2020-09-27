d9bee14cbce1c60368ec0ba88e9d68800a4a6527
Akbar <akbarpulatov7@gmail.com>
2020-09-27 17:40:36 +0500

    timer is added

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Http
+using System.Net.Http;
+
+//Timer
+using System.Threading;
+using System.Windows.Threading;
+
+namespace navbat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer = null;
+        private int x;
+
+        private void timerStart()
+        {
+            timer = new DispatcherTimer();  // если надо, то в скобках указываем приоритет, например DispatcherPriority.Render
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            timer.Start();
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Timer is Fired!");
+            x++;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timerStart();
+        }
+
+
+    }
+}
