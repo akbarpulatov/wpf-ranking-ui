223dd0d290a517144a3a1ee665a73f53ccd5a051
akbar <akbarpulatov7@gmail.com>
2020-09-28 13:38:36 +0500

    alohida threadga olib o'tdim

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,7 @@ using System.Net;
 using System.IO;
 
 using Newtonsoft.Json;
+using System.Net.Sockets;
 
 namespace navbat
 {
@@ -71,7 +72,10 @@ namespace navbat
         {
             Console.WriteLine("Timer is Fired!");
             x++;
-            makeHttp();
+            //makeHttp();
+            //DoAcceptTcpClientCallback();
+            //MyTcpListener.mymain();
+
         }
 
         public void makeHttp()
@@ -86,10 +90,17 @@ namespace navbat
             }
         }
 
+        Thread thread = new Thread(() =>
+        {
+            // put the code here that you want to be executed in a new thread
+            MyTcpListener.mymain();
+        });
+
         public MainWindow()
         {
             InitializeComponent();
-            //timerStart();
+
+            timerStart();
 
             //data[0].name = "Akbar";
 
@@ -123,6 +134,10 @@ namespace navbat
             loopTimer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
             loopTimer.Start();
 
+
+            // tcp
+            thread.Start();
+            // MyTcpListener.mymain();
         }
 
         //shuhrat
@@ -170,6 +185,80 @@ namespace navbat
         }
 
 
+    }
+
+    class MyTcpListener
+    {
+        public static void mymain()
+        {
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 80;
+                IPAddress localAddr = IPAddress.Parse("0.0.0.0");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        //data = data.ToUpper();
+
+                        //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        //// Send back a response.
+                        //stream.Write(msg, 0, msg.Length);
+                        //Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
     }
 
 }
